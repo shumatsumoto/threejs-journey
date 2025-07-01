81cf3b4fbdf1ab5b92e19d9adc31172e17d6e3f5
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-07-01 22:43:02 +0900

    20.物理学

--- 20/src/script.js
+++ 20/src/script.js
@@ -38,18 +38,38 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+// Materials
+const defaultMaterial = new CANNON.Material("default");
+// const plasticMaterial = new CANNON.Material("plastic");
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(defaultContactMaterial);
+
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: defaultMaterial,
 });
+sphereBody.applyLocalForce(
+  new CANNON.Vec3(150, 0, 0),
+  new CANNON.Vec3(0, 0, 0)
+);
 world.addBody(sphereBody);
 
 // Floor
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
+floorBody.material = defaultMaterial;
 floorBody.mass = 0;
 floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI * 0.5);
@@ -167,6 +187,7 @@ const tick = () => {
   oldElapsedTime = elapsedTime;
 
   // Update physics world
+  sphereBody.applyForce(new CANNON.Vec3(-5, 0, 0), sphereBody.position);
   world.step(1 / 60, deltaTime, 3);
 
   sphere.position.copy(sphereBody.position);
